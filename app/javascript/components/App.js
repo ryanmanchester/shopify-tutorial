31804f12bb91b6e13c5bd5d4d9093d9b30917952
Ryan Manchester <ryanm17@gmail.com>
2021-03-31 22:07:52 -0400

    set up a sample shopify interface

---
+++ app/javascript/components/App.js
@@ -0,0 +1,35 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { AppProvider, EmptyState, Page } from '@shopify/polaris';
+import { authenticatedFetch } from '@shopify/app-bridge-utils';
+
+import enTranslations from '@shopify/polaris/locales/en.json';
+import React from 'react';
+import TestData from './TestData'
+
+export default function App() {
+  const client = new ApolloClient({
+    link: new HttpLink({
+      credentials: 'same-origin',
+      fetch: authenticatedFetch(window.app), // created in shopify_app.js
+      uri: '/graphql'
+    }),
+    cache: new InMemoryCache()
+  });
+
+  return (
+    <AppProvider i18n={enTranslations}>
+      <ApolloProvider client={client}>
+        <Page>
+          <EmptyState>
+            <TestData />
+          </EmptyState>
+        </Page>
+      </ApolloProvider>
+    </AppProvider>
+  );
+}
